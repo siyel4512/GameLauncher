a68e4e0f60c48449393ff4595582170f8068f73e
SANGHALEE <sangha206@naver.com>
2023-07-11 17:02:19 +0900

    set notice + set banner

---
+++ Assets/02. Scripts/9. Event & Notice  & Inform/BannerInfo.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BannerInfo : MonoBehaviour
+{
+    public string title;
+    //public string content;
+    public string linkURL;
+
+    public BannerUI bannerUI;
+
+    public TMP_Text title_text;
+    //public TMP_Text content_text;
+
+    public EventTrigger.Entry clickEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetEvent();
+    }
+
+    // set contents
+    public void SetContents(string _title, string _linkURL)
+    {
+        title = _title;
+        //content = _content;
+        linkURL = _linkURL;
+
+        title_text.text = title;
+        //content_text.text = content;
+    }
+
+    // open url
+    public void BTN_OpenURL()
+    {
+        Application.OpenURL(linkURL);
+
+        if (bannerUI.isTimer)
+        {
+            bannerUI.timer.Stop();
+            bannerUI.timer.Start();
+        }
+        else
+        {
+            bannerUI.sw.Restart();
+        }
+    }
+
+    // click event to eventtrigger
+    public void SetEvent()
+    {
+        clickEvent = new EventTrigger.Entry();
+        clickEvent.eventID = EventTriggerType.PointerClick;
+        clickEvent.callback.AddListener((eventData) => BTN_OpenURL());
+
+        GetComponent<EventTrigger>().triggers.Add(clickEvent);
+    }
+}
